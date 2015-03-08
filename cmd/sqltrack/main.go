807b5f6d7658b7271c702235c649be28f9343c74
dongzerun <xueling1000@sina.com>
2015-03-08 12:34:14 +0800

    add protobuf

--- cmd/sqltrack/main.go
+++ cmd/sqltrack/main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/dongzerun/sqltrack/kafka"
+	"github.com/dongzerun/sqltrack/message"
+	"github.com/golang/protobuf/proto"
 	"log"
 	"os"
 	"os/signal"
@@ -14,29 +16,30 @@ var (
 	configPath = flag.String("config", "/tmp/kafka.toml", "config must be file")
 )
 
-// [kafkaInput]
-// topic = "mysql.slow.ms"
-// addrs = ["g1-db-srv-00:9092", "g1-db-srv-01:9092", "g1-db-srv-02:9092"]
-// encoder = "ProtobufEncoder"
-// partitioner = "Random"
-// offsetmethod = "Manual"
-
 func main() {
 
 	flag.Parse()
 	globals := loadConfig(configPath)
 	log.Println(globals.KafkaConfig)
 	log.Println(globals.KafkaConfig.Addrs)
-	// ConsumerWithSelect()
 
 	kh := kafka.NewKafkaHelper(globals.KafkaConfig)
 	go kh.StartPull()
-	for {
-		select {
-		case data := <-kh.MsgKafka:
-			fmt.Println(data.Topic, data.Key, data.Offset, data.Partition)
+
+	kh.Wg.Wrap(func() {
+		for {
+			select {
+			case data := <-kh.MsgKafka:
+				fmt.Println(data.Topic, data.Key, data.Offset, data.Partition)
+				msg := &message.Message{}
+				proto.Unmarshal(data.Value, msg)
+				fmt.Println(msg)
+			case <-kh.StopChan:
+				return
+			}
 		}
-	}
+	})
+
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc,
 		os.Kill,
